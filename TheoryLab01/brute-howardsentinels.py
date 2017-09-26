06598e33b6a842c28fb427755405c7fddb5c85eb
Lauren G Tucker <ltucker@nd.edu>
2017-09-25 23:39:28 -0400

    P1V1

--- TheoryLab01/brute-howardsentinels.py
+++ TheoryLab01/brute-howardsentinels.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python2.7
 
 import time
+import sys
 
+# parse command line arguments for filename and binary argument
+args = sys.argv[1:]
+arg = args.pop(0)
+FILENAME = arg
+arg = args.pop(0)
+if arg == '0':
+  DEBUG = False
+else:
+  DEBUG = True
 
+# reads in the next wff from the specified input file
+def readwff(listoflines):
+  d = listoflines.rsplit(" ")
+  c = d.pop(0)
+  ProbNum = d.pop(0)
+  while (c != 'c'):
+  	parse until find another c
+
+  return tuple of output values
+     
+
+f = open(FILENAME, "r").readlines()
+
+f = readwff(f, 1)
+print f
