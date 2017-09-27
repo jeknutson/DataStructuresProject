9baa9093e0d8b54d195f1fe120aa9b4c3be7e0c7
Jacob Knutson <jknutson@nd.edu>
2017-09-26 20:42:45 -0400

    first function maybe done

--- TheoryLab01/brute-howardsentinels.py
+++ TheoryLab01/brute-howardsentinels.py
@@ -14,17 +14,32 @@ else:
   DEBUG = True
 
 # reads in the next wff from the specified input file
-def readwff(listoflines):
-  d = listoflines.rsplit(" ")
-  c = d.pop(0)
-  ProbNum = d.pop(0)
-  while (c != 'c'):
-  	parse until find another c
+def readwff(f):
+  
+    # Get first line of wff, store each value in a list
+    line = f.readline().rstrip().split(" ")
 
-  return tuple of output values
+    probNum = line[1]
+    maxLit = line[2]
+    satVal = line[3]
+
+    line = f.readline().rstrip().split(" ")
+    numVar = line[2]
+    numClauses = line[3]
+
+    # Iterate through clauses and store into list
+    L = []
+    for i in range(int(numClauses)):
+	#0 is actually the delimeter but project document says to assume on 1 line
+        line = f.readline().rstrip().split(",")
+        # We want each literal of the clause but not the delimeter 0
+        L.append(line[0:len(line)-1])
+        
+    # Return values in a tuple
+    return (probNum, maxLit, satVal, numVar, numClauses, L)
      
 
-f = open(FILENAME, "r").readlines()
+f = open(FILENAME, "r")
 
-f = readwff(f, 1)
-print f
+v = readwff(f)
+print v
